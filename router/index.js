ebfd6a496224d03126ba2f0a01409c9fc28d94b6
Enzo Livrari <ti02.awf@gmail.com>
2024-09-18 15:31:19 -0300

    Update index.js

--- router/index.js
+++ router/index.js
@@ -1,16 +1,37 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import EscolasPage from '@/components/EscolasPage.vue';
-
-
-Vue.use(VueRouter);
+import { createRouter, createWebHistory } from 'vue-router';
+import EscolasPage from '../components/EscolasPage.vue';
+import AlunosPage from '../components/AlunosPage.vue';
+import OrganizacaoPage from '../components/OrganizacaoPage.vue';
 
 const routes = [
-  { path: '/', component: EscolasPage },
+  {
+    path: '/escolas',
+    name: 'escolas',
+    component: EscolasPage
+  },
+  {
+    path: '/alunos',
+    name: 'alunos',
+    component: AlunosPage
+  },
+  {
+    path: '/organizacao',
+    name: 'organizacao',
+    component: OrganizacaoPage
+  },
+  {
+    path: '/',
+    redirect: '/escolas'
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/'
+  }
 ];
 
-const router = new VueRouter({
-  routes,
+const router = createRouter({
+  history: createWebHistory(),
+  routes
 });
 
 export default router;
