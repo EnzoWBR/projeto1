3a6de3fd1ba649ecee5fdac63b12d37bed0eec20
Enzo Livrari <ti02.awf@gmail.com>
2024-09-23 17:23:27 -0300

    Update aulas_controller.ts

--- controllers/aulas_controller.ts
+++ controllers/aulas_controller.ts
@@ -1,86 +1,93 @@
-import Aulas from '#models/aula'
-import type { HttpContext } from '@adonisjs/core/http'
+import Aulas from '#models/aula';
+import type { HttpContext } from '@adonisjs/core/http';
 
 export default class AulasController {
-    async index({ response }: HttpContext) {
-        try {
-          const aula = await Aulas.all()
-          if (aula.length) {
-            response.safeStatus(200).json({
-              aula: aula,
-            })
-          } else {
-            response.safeStatus(200).json({
-              message: `Não ha aula para listar!`,
-            })
-          }
-        } catch (error) {
-          response.safeStatus(400).json({
-            message: `Erro ao consultar a aula! ${error}`,
-          })
-        }
+  async index({ response }: HttpContext) {
+    try {
+      const aulas = await Aulas.all();
+      if (aulas.length) {
+        response.safeStatus(200).json({
+          aulas: aulas,
+        });
+      } else {
+        response.safeStatus(200).json({
+          message: `Não há aulas para listar!`,
+        });
       }
-    
-      async store({ request, response }: HttpContext) {
-        const { ...data } = request.all()
-    
-        try {
-          const aulaAdd = await Aulas.create(data)
-    
-          return response.safeStatus(201).json({
-            message: `aula ${aulaAdd.titulo}, criada com sucesso!`,
-          })
-        } catch (error) {
-          response.safeStatus(400).json({
-            message: `Erro ao incluir a aula!${error}`,
-          })
-        }
-      }
-    
-      async show({ params, response }: HttpContext) {
-        try {
-          const aulaF = await Aulas.find(params.id)
-    
-          if (aulaF !== null) {
-            return response.safeStatus(200).json({
-              aulaF: aulaF,
-            })
-          } else {
-            return response.safeStatus(200).json({
-              message: `aula não localizada!`,
-            })
-          }
-        } catch (error) {
-          response.safeStatus(400).json({
-            message: `Erro ao consultar a aula`,
-          })
-        }
-      }
-    
-      async update({ params, request, response }: HttpContext) {
-        try {
-          const aulita= await Aulas.findOrFail(params.id)
-    
-          const { ...data } = request.only([
-            'titulo',
-            'conteudo',
-            'arquivo',
-            'createdAt',
-            'updatedAt',
-          ])
-    
-          aulita.merge(data)
-    
-          const aulaModified = await aulita.save()
-    
-          return response.safeStatus(201).json({
-            message: `aula ${aulaModified.titulo}, alterado com sucesso!`,
-          })
-        } catch (error) {
-          response.safeStatus(400).json({
-            message: `Erro ao alterar a aula! ${error}`,
-          })
-        }
-      }
+    } catch (error) {
+      response.safeStatus(400).json({
+        message: `Erro ao consultar as aulas! ${error}`,
+      });
     }
-    
+  }
+
+  async store({ request, response }: HttpContext) {
+    const { titulo, conteudo, arquivo } = request.only(['titulo', 'conteudo', 'arquivo']);
+
+    try {
+      const aulaAdd = await Aulas.create({ titulo, conteudo, arquivo });
+
+      return response.safeStatus(201).json({
+        message: `Aula ${aulaAdd.titulo}, criada com sucesso!`,
+      });
+    } catch (error) {
+      response.safeStatus(400).json({
+        message: `Erro ao incluir a aula! ${error}`,
+      });
+    }
+  }
+
+  async show({ params, response }: HttpContext) {
+    try {
+      const aulaF = await Aulas.find(params.id);
+
+      if (aulaF !== null) {
+        return response.safeStatus(200).json({
+          aulaF: aulaF,
+        });
+      } else {
+        return response.safeStatus(200).json({
+          message: `Aula não localizada!`,
+        });
+      }
+    } catch (error) {
+      response.safeStatus(400).json({
+        message: `Erro ao consultar a aula`,
+      });
+    }
+  }
+
+  async update({ params, request, response }: HttpContext) {
+    try {
+      const aula = await Aulas.findOrFail(params.id);
+
+      const { titulo, conteudo, arquivo } = request.only(['titulo', 'conteudo', 'arquivo']);
+
+      aula.merge({ titulo, conteudo, arquivo });
+
+      await aula.save();
+
+      return response.safeStatus(201).json({
+        message: `Aula ${aula.titulo}, alterada com sucesso!`,
+      });
+    } catch (error) {
+      response.safeStatus(400).json({
+        message: `Erro ao alterar a aula! ${error}`,
+      });
+    }
+  }
+
+  async destroy({ params, response }: HttpContext) {
+    try {
+      const aula = await Aulas.findOrFail(params.id);
+      await aula.delete();
+      return response.safeStatus(200).json({
+        message: `Aula ${aula.titulo} excluída com sucesso!`,
+      });
+    } catch (error) {
+      response.safeStatus(400).json({
+        message: `Erro ao excluir a aula! ${error}`,
+      });
+    }
+  }
+}
